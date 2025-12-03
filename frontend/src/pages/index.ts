db7b721a68400c4f5c6afb4a6c794f919cf41225
Adam Refaey <dev.adam.refaey@gmail.com>
2025-12-03 21:36:06 +0200

    Add index file to export main application pages: Dashboard, Queues, Settings, Tasks, and Workers

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as Queues } from "./Queues";
+export { default as Settings } from "./Settings";
+export { default as Tasks } from "./Tasks";
+export { default as Workers } from "./Workers";
