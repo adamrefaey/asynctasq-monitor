ed61df34702865c9a86c2927eaa7b6dae7c40584
Adam Refaey <dev.adam.refaey@gmail.com>
2025-12-03 21:36:25 +0200

    Refactor main entry point to use QueryProvider and RouterProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { QueryProvider } from "@/lib/query";
+import { router } from "@/router";
 import "./tailwind.css";
-import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<App />
+			<QueryProvider>
+				<RouterProvider router={router} />
+			</QueryProvider>
 		</StrictMode>,
 	);
 }
